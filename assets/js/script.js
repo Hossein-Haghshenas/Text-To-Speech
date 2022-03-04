15748a0fb31876775a164e704267db0311c4b0c3
Hossein <haghshenas3c@gmail.com>
2022-03-04 12:57:43 +0330

    Update#1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
+const description = document.querySelector("#description");
 const text = document.querySelector("#text-input");
 const button = document.querySelector("#play-btn");
 const buttonIcon = document.querySelector("#play-icon");
-
+const buttonText = buttonIcon.nextElementSibling;
 /* create a new speech and set something */
 
 const speech = new SpeechSynthesisUtterance();
@@ -9,15 +10,20 @@ speech.lang = "en-US";
 speech.volume = 0.5;
 
 speech.onstart = () => {
+  buttonText.textContent = "Pause";
   buttonIcon.classList.replace("fa-play", "fa-pause");
 };
 speech.onend = () => {
+  buttonText.textContent = "Play";
   buttonIcon.classList.replace("fa-pause", "fa-play");
 };
 
 button.addEventListener("click", () => {
   /* get and set text  */
-  speech.text = text.value;
-  /* play speech */
-  window.speechSynthesis.speak(speech);
+  text.value === ""
+    ? (description.textContent = "Please enter something !")
+    : (speech.text = text.value) &&
+      (description.textContent = "Type here then press the play button") &&
+      /* play speech */
+      window.speechSynthesis.speak(speech);
 });
